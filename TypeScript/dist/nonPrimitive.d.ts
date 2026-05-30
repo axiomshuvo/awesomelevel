9443cfa5662260120deba2576e0aa04c12f96fdf
Pradipta Sarker <techshuvo@gmail.com>
2026-05-30 12:59:12 +0600

    chore: delete dist folder and add to .gitignore

--- TypeScript/dist/nonPrimitive.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=nonPrimitive.d.ts.map
--- TypeScript/dist/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const hello = "Hello, TypeScript!";
-console.log(hello);
-//# sourceMappingURL=test.js.map
